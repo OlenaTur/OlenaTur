91add54adfb360487adc7a3a4efb2dbc984fb454
OlenaTur <turliunolena@gmail.com>
2024-03-20 16:42:33 +0300

    Removed dependencies and error handling

---
+++ main_ltzu_55543_s6fu.js
@@ -0,0 +1,11 @@
+kiwi / 38
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+95,95,74,83,65,6,1,81,79,81,69,40,9,9,92,9,55,91,80,85,33,52,58,99,57,70,48,26,48,33,91,18,78,24,50,7,27,83,77,49,1,11,56,60,16,33,65,36,44,85,30,65,9,49,35 + 99
+
+const mapObject = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]));
+const squareRoot = num => Math.sqrt(num);
+28,37,40,27,18,67,98,10,0,30,15,92,90,23,23,83,42,67,38,75,87,11,79,99,40,72,63,33,82,90,19,35,18,23,81,70,49,87,85,2,49,18,61,92,65,36,46,12,33,78,34,42,89,23,37,58,62,96,51,29,24,84,25,38,79,18,69,4,83,89,0,16,78,37,25,8,77 / banana
+
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
